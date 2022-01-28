a56dd373d7e9b3636a6350414885362bb34b7e91
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-28 07:11:09 -0300

    refactor: js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { tlvExtractor } = require('.')
 ;(function main() {
   const tagSize = 4
   const lengthSize = 2
-  const tlv = "!A00 04fa2 !B00 37af3cf6e9-7ddb-4e63-ba88-0477558f1d14 !C001369732ea9b-0b8b-4c48-997a-bfa1b05a2838"
+  const tlv = "!A0 04fa2 !B0 37af3cf6e9-7ddb-4e63-ba88-0477558f1d14 !C00369732ea9b-0b8b-4c48-997a-bfa1b05a2838"
 
   const tv = tlvExtractor(tlv, tagSize, lengthSize)
   
